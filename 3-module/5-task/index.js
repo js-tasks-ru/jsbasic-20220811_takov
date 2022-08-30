79c848a17f48e8e886a9cd9b323a7da907f3c9db
Illya Abesadze <illyaabesadze@MBP-Illya.homenet.telecomitalia.it>
2022-08-30 10:11:42 +0200

    GetMinMax is fixed

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -3,16 +3,27 @@ const inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
 const minMaxObj = {}
 
 
+// function getMinMax(str) {
+
+//   let arr = str
+//     .split(' ')
+//     .filter((arg) => Number.isInteger(parseInt(arg)))
+//     .map((arg) => arg.includes('.') ? parseFloat(arg) : parseInt(arg))
+//     .sort((a, b) => a - b)
+//   return { min: arr[0], max: arr[arr.length - 1] }
+
+// }
+
 function getMinMax(str) {
-  
+
   let arr = str
     .split(' ')
     .filter((arg) => Number.isInteger(parseInt(arg)))
     .map((arg) => arg.includes('.') ? parseFloat(arg) : parseInt(arg))
-    .sort((a, b) => a - b)
-  return { min: arr[0], max: arr[arr.length - 1] }
+  return { min: Math.min.apply(0, arr), max: Math.max.apply(0, arr) }
 
 }
+
 
 
 
