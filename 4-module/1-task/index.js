5619cc67bfc6202b9d3f9805cd096cdc5262ac8c
Illya Abesadze <illyaabesadze@MBP-Illya.homenet.telecomitalia.it>
2022-09-04 14:16:42 +0200

    All done

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,50 @@
+let friends = [
+  {
+    firstName: 'Artsiom',
+    lastName: 'Mezin'
+  },
+  {
+    firstName: 'Ilia',
+    lastName: 'Kantor'
+  },
+  {
+    firstName: 'Christopher',
+    lastName: 'Michael'
+  }
+];
+
+
+
+
 function makeFriendsList(friends) {
-  // ваш код...
+
+  let list = document.createElement('ul')
+  document.body.append(list)
+  
+  for (let i = 0; i < friends.length; i++) {
+    let item = document.createElement('li')
+    item.append(`${friends[i].firstName + ' ' + friends[i].lastName}`)
+    list.append(item)
+  }
+
+  return list
+
 }
+
+
+// function makeFriendsList(friends) {
+
+//   let list = document.createElement('ul')
+//   document.body.append(list)
+  
+//   for (let i = 0; i < friends.length; i++) {
+//     let item = document.createElement('li')
+//     item.innerHTML = `${friends[i].firstName + ' ' + friends[i].lastName}`
+//     list.append(item)
+//   }
+
+//   return list
+
+// }
+
+document.body.appendChild(makeFriendsList(friends));
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,50 @@
+const table = document.querySelector(".js-teachers");
+
 function highlight(table) {
-  // ваш код...
+
+  const headCells = table.tHead.rows[0].cells;
+  let indexStatus;
+  let indexGender;
+  let indexAge;
+
+  for (let i = 0; i < headCells.length; i++) {
+    if (headCells[i].innerText.toLowerCase() === 'status') {
+      indexStatus = i;
+    }
+    if (headCells[i].innerText.toLowerCase() === 'gender') {
+      indexGender = i;
+    }
+    if (headCells[i].innerText.toLowerCase() === 'age') {
+      indexAge = i;
+    }
+  }
+
+  for (let row of table.tBodies[0].rows) {
+    if (!row.cells[indexStatus].hasAttribute('data-available')) {
+      row.classList.add('hidden');
+    }
+
+    if (row.cells[indexGender].innerText.toLowerCase() === 'm') {
+      row.classList.add('male');
+    }
+
+    if (row.cells[indexGender].innerText.toLowerCase() === 'f') {
+      row.classList.add('female');
+    }
+
+    if (row.cells[indexStatus].getAttribute('data-available') === 'true') {
+      row.classList.add('available');
+    } 
+    
+    if (row.cells[indexStatus].getAttribute('data-available') === 'false'){
+      row.classList.add('unavailable');
+    }
+
+    if (parseInt(row.cells[indexAge].innerText) < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
+
+
+highlight(table);
